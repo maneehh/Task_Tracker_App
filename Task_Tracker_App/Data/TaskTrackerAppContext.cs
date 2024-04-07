404426d617876e9445f3145e1d000ebee25dd4bf
Acer <Acer@DESKTOP-AV8L5D2>
2024-04-07 18:52:31 +0400

    Add project files.

---
+++ Task_Tracker_App/Data/TaskTrackerAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models = Task_Tracker_App.Models;
+
+namespace Task_Tracker_App.Data
+{
+    public class TaskTrackerAppContext : DbContext
+    {
+        public DbSet<Models.Task> Tasks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-AV8L5D2\\SQLEXPRESS;Initial Catalog=TaskTrackerApp;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
---
+++ Task_Tracker_App/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_Tracker_App.Models
+{
+    public class Task
+    {
+
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
